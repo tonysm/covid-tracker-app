aef7ab8251ef38cbff88ca0c20844a835786aa51
Tony Messias <tonyzrp@gmail.com>
2020-03-18 22:40:30 -0300

    Tweak styles

--- resources/views/_nav.blade.php
+++ resources/views/_nav.blade.php
@@ -1,10 +1,10 @@
 <div class="p-6 mb-6 shadow">
     <nav class="container mx-auto flex items-center justify-between flex-wrap">
         <div class="flex items-center flex-shrink-0 text-black font-bold mr-6">
-            <span class="font-semibold text-3xl tracking-tight">COVID-19 Tracker</span>
+            <span class="font-semibold font-mono text-3xl tracking-tight">COVID-19 Tracker</span>
         </div>
         <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <button class="flex items-center px-3 py-2 border text-teal-200 border-teal-400 hover:text-white hover:border-white">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
@@ -15,13 +15,13 @@
                 </a>
             </div>
             <div class="flex">
-                <a href="#" class="mr-2 flex items-center inline-block text-sm px-4 py-2 leading-none border rounded text-gray-800 border-gray-700 hover:border-transparent hover:bg-gray-800 hover:text-gray-200 mt-4 lg:mt-0">
+                <a href="#" class="mr-2 flex items-center inline-block text-sm px-4 py-2 leading-none border text-gray-800 border-gray-700 hover:border-transparent hover:bg-gray-800 hover:text-gray-200 mt-4 lg:mt-0">
                     <svg class="mr-1 h-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M9.26 13a2 2 0 0 1 .01-2.01A3 3 0 0 0 9 5H5a3 3 0 0 0 0 6h.08a6.06 6.06 0 0 0 0 2H5A5 5 0 0 1 5 3h4a5 5 0 0 1 .26 10zm1.48-6a2 2 0 0 1-.01 2.01A3 3 0 0 0 11 15h4a3 3 0 0 0 0-6h-.08a6.06 6.06 0 0 0 0-2H15a5 5 0 0 1 0 10h-4a5 5 0 0 1-.26-10z"/>
                     </svg>
                     Source Code
                 </a>
-                <a href="#" class="flex items-center inline-block text-sm px-4 py-2 leading-none border rounded text-gray-800 border-gray-700 hover:border-transparent hover:bg-gray-800 hover:text-gray-200 mt-4 lg:mt-0">
+                <a href="#" class="flex items-center inline-block text-sm px-4 py-2 leading-none border text-gray-800 border-gray-700 hover:border-transparent hover:bg-gray-800 hover:text-gray-200 mt-4 lg:mt-0">
                     <svg class="mr-1 h-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M9.26 13a2 2 0 0 1 .01-2.01A3 3 0 0 0 9 5H5a3 3 0 0 0 0 6h.08a6.06 6.06 0 0 0 0 2H5A5 5 0 0 1 5 3h4a5 5 0 0 1 .26 10zm1.48-6a2 2 0 0 1-.01 2.01A3 3 0 0 0 11 15h4a3 3 0 0 0 0-6h-.08a6.06 6.06 0 0 0 0-2H15a5 5 0 0 1 0 10h-4a5 5 0 0 1-.26-10z"/>
                     </svg>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,36 +2,36 @@
 
 @section('content')
     <div class="container px-6 sm:p-0 mx-auto">
-        <h2 class="text-lg sm:text-2xl font-bold py-2">Confirmed Cases Around the World</h2>
+        <h2 class="text-lg sm:text-6xl text-center font-sans font-bold py-4">Confirmed Cases Around the World</h2>
         <div class="sm:grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 sm:-mx-2">
             @foreach(range(1, 20) as $index)
                 <div class="sm:m-2 sm:max-w-sm rounded overflow-hidden shadow-lg">
                     <div class="px-6 pt-4">
                         <div class="font-bold text-xl mb-2">{{ rand(0, 1) ? 'Brazil' : 'Belgium' }}</div>
                     </div>
-                    <div class="pb-4 text-center text-2xl">
-                        500
+                    <div class="pb-4 text-center text-5xl">
+                        {{ rand(100, 90000) }}
                     </div>
                 </div>
             @endforeach
         </div>
-        <h2 class="text-2xl font-bold py-2 mt-6">Deaths Around the World</h2>
+        <h2 class="text-lg sm:text-6xl text-center font-sans font-bold py-4">Deaths Around the World</h2>
         <div class="sm:grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 sm:-mx-2">
             @foreach(range(1, 20) as $index)
                 <div class="sm:m-2 sm:max-w-sm rounded overflow-hidden shadow-lg">
                     <div class="px-6 pt-4">
                         <div class="font-bold text-xl mb-2">{{ rand(0, 1) ? 'Brazil' : 'Belgium' }}</div>
                     </div>
-                    <div class="pb-4 text-center text-2xl">
-                        500
+                    <div class="pb-4 text-center text-5xl">
+                        {{ rand(100, 200) }}
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
 
-    <footer class="mt-4 bg-gray-200 h-20 flex items-center justify-center border shadow font-mono">
-        <p>
+    <footer class="mt-6 bg-gray-200 border-t border-gray-400 h-20 flex items-center justify-center font-mono">
+        <p class="text-gray-900">
             By: Tony Messias
         </p>
     </footer>
